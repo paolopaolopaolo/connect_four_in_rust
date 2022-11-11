5a57f13fb627fcaaa3457d1849a4667ab7939e8e
Dean Mercado <dean@yeti.co>
2022-11-10 23:22:09 -0800

    Fix a dumb bug

--- src/main.rs
+++ src/main.rs
@@ -37,9 +37,13 @@ fn board_render_thread (rx: Receiver<Message>) -> JoinHandle<(crossterm::Result<
                 _ => ()
             }
             print_board(&gameboard).unwrap();
-            if gameboard.is_winner(&CellState::O) || gameboard.is_winner(&CellState::X) {
-                println!("{} wins! Press q to quit.\nPress n for new game.", CellState::O);
-            } else if gameboard.is_full() {
+            if gameboard.is_winner(&CellState::O) {
+                println!("{} wins! Press q to quit.\r\nPress n for new game.", CellState::O);
+            } else if gameboard.is_winner(&CellState::X) {
+                println!("{} wins! Press q to quit.\r\nPress n for new game.", CellState::X);
+            }
+            else if gameboard.is_full() {
+                println!("Nobody won. Drop another piece to start a new game.");
                 gameboard.clear();
             }
         }
@@ -56,6 +60,7 @@ fn board_sender_thread (tx: Sender<Message>) -> crossterm::Result<()>{
                     KeyCode::Left => {tx.send(Message::MoveCursor(-1)).unwrap();},
                     KeyCode::Right => {tx.send(Message::MoveCursor(1)).unwrap();},
                     KeyCode::Enter => {tx.send(Message::PlacePiece).unwrap();},
+                    KeyCode::Down => {tx.send(Message::PlacePiece).unwrap();},
                     KeyCode::Char('n') => {tx.send(Message::Reset).unwrap();},
                     KeyCode::Char('q') => { terminal::disable_raw_mode().expect("error"); println!("\rYou can now press CTRL + C to exit."); break;},
                     _ => (),
